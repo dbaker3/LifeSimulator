01dcd989dfa3e32c732cf64d813bd3988d53667a
David Baker <dbaker3@jimbomail.com>
2013-10-08 19:40:49 -0400

    Created project

---
+++ Board.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimulator
+{
+    static class Board
+    {
+        static Board()
+        {
+            
+        }
+
+        public static int Height { get; set; }
+        public static int Width { get; set; }
+
+        public enum CellStates { unpopulated, populated }
+        public enum BoardStates { running, stopped }
+        
+        
+
+
+    }
+}
---
+++ Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeSimulator
+{
+    class Cell
+    {
+        public Cell(Board.CellStates cs)
+        {
+            MyState = cs;
+        }
+
+        private Board.CellStates _myState;
+        public Board.CellStates MyState
+        {
+            get { return _myState; }
+            set { _myState = value; }
+        }
+        
+        
+
+
+    }
+}
